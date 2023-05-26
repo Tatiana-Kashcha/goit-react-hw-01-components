37a95bdbd8f4af6685583959f6c3d3b68cc55c1c
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-26 23:47:39 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,37 +1,67 @@
 import user from '../../data/user.json';
 
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>;
+// {
+/* <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />; */
+// }
 
-<div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    // {/* <div class="profile"> */}
+    <Container>
+      <Description>
+        {/* <div class="description"> */}
+        <Avatar
+          // <img
+          //   src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          src={avatar}
+          alt="User avatar"
+          //   class="avatar"
+          // />
+        />
+        <Name>{username}</Name>
+        {/* <p class="name">Petra Marica</p> */}
+        <Tag>@{tag}</Tag>
+        {/* <p class="tag">@pmarica</p> */}
+        <Location>{location}</Location>
+        {/* <p class="location">Salvador, Brasil</p> */}
+        {/* </div> */}
+      </Description>
 
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div>;
+      <ListsStats>
+        {/* <ul class="stats"> */}
+        <li>
+          <Label>Followers</Label>
+          {/* <span class="label">Followers</span> */}
+          <Quantity>{followers}</Quantity>
+          {/* <span class="quantity">1000</span> */}
+        </li>
+        <li>
+          <Label>Views</Label>
+          {/* <span class="label">Views</span> */}
+          <Quantity>{views}</Quantity>
+          {/* <span class="quantity">2000</span> */}
+        </li>
+        <li>
+          <Label>Likes</Label>
+          {/* <span class="label">Likes</span> */}
+          <Quantity>{likes}</Quantity>
+          {/* <span class="quantity">3000</span> */}
+        </li>
+        {/* </ul> */}
+      </ListsStats>
+    </Container>
+    // {/* </div>; */}
+  );
+};
