b99f693f89081938428baf7d5e8ab10cbf58de1c
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-27 11:57:40 +0300

    add style profile

--- src/components/Profile/Profile.styled.js
+++ src/components/Profile/Profile.styled.js
@@ -1,10 +1,31 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 300px;
+  box-shadow: 0px 10px 10px rgba(245, 245, 247, 0.1);
+  border-radius: 16px;
+`;
 
-export const Description = styled.div``;
+export const Description = styled.div`
+  background-color: #ffffff;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+`;
 
-export const Avatar = styled.img``;
+export const Avatar = styled.img`
+  width: 100px;
+  border-radius: 100%;
+  background-color: rgb(227 244 244);
+`;
 
 export const Name = styled.p``;
 
@@ -12,9 +33,24 @@ export const Tag = styled.p``;
 
 export const Location = styled.p``;
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  background-color: rgb(227 244 244);
+  border: 1px solid rgb(219, 216, 216);
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
 
-export const ListItems = styled.li``;
+export const ListItems = styled.li`
+  text-align: center;
+  flex-direction: column;
+  display: flex;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  width: 100px;
+  border-right: 1px solid rgb(219, 216, 216);
+`;
 
 export const Label = styled.span``;
 
