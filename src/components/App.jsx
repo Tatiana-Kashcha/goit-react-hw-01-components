f2701ce8af884bc2d350e7b095cc602fff03e926
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-28 16:25:32 +0300

    add stat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,21 @@
 import { Profile } from 'components/Profile/Profile';
 import user from 'data/user.json';
 
+import { Statistics } from 'components/Statistics/Statistics';
+import data from 'data/data.json';
+
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+    </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,42 @@
+const Mailbox = ({ unreadMessages }) => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 && (
+        <p>You have {unreadMessages.length} unread messages.</p>
+      )}
+    </div>
+  );
+};
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section class="statistics">
+      {title(<h2 class="title">Upload stats</h2>)}
+      <ul class="stat-list"></ul>
+    </section>
+  );
+};
+
+<section class="statistics">
+  <h2 class="title">Upload stats</h2>
+
+  <ul class="stat-list">
+    <li class="item">
+      <span class="label">.docx</span>
+      <span class="percentage">4%</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp3</span>
+      <span class="percentage">14%</span>
+    </li>
+    <li class="item">
+      <span class="label">.pdf</span>
+      <span class="percentage">41%</span>
+    </li>
+    <li class="item">
+      <span class="label">.mp4</span>
+      <span class="percentage">12%</span>
+    </li>
+  </ul>
+</section>;
