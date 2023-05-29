bb87d0fbc31ba2f45bd20b2aea821ba4f3faa4c7
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-30 01:43:42 +0300

    Update FriendListItem.styled.js

--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -4,16 +4,16 @@ export const ListItems = styled.li`
   background-color: #ffffff;
   display: flex;
   align-items: center;
+  padding: 5px 10px;
   margin-bottom: 10px;
   box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
     0px 2px 1px rgba(0, 0, 0, 0.2);
 `;
 
-export const Avatar = styled.img`
-  border-radius: 4px;
-`;
+export const Avatar = styled.img``;
 
 export const Name = styled.p`
+  font-size: 20px;
   font-weight: bold;
 `;
 
