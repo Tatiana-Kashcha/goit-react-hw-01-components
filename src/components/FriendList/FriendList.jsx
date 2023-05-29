8dffc7c8decfbdbec99be163cc55379e8290d194
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-30 00:59:06 +0300

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,7 +5,13 @@ export const FriendList = ({ friends }) => {
   return (
     <s.List>
       {friends.map(friend => (
-        <FriendListItem key={friend.id} />
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+          id={friend.id}
+        />
       ))}
     </s.List>
   );
