b497b3ec9f71b24d1f04a599b1b14f8c700682a7
Tatiana Kashcha <tkashcha@gmail.com>
2023-05-30 12:14:55 +0300

    add status

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import * as s from './FriendListItem.styled';
 export const FriendListItem = ({ avatar, name, isOnline, id }) => {
   return (
     <s.ListItems key={id}>
-      <s.Status>{isOnline}</s.Status>
+      <s.Status status={isOnline}></s.Status>
       <s.Avatar src={avatar} alt={name} width="48" height="48" />
       <s.Name>{name}</s.Name>
     </s.ListItems>
--- src/components/FriendListItem/FriendListItem.styled.js
+++ src/components/FriendListItem/FriendListItem.styled.js
@@ -10,7 +10,9 @@ export const ListItems = styled.li`
     0px 2px 1px rgba(0, 0, 0, 0.2);
 `;
 
-export const Avatar = styled.img``;
+export const Avatar = styled.img`
+  margin-right: 10px;
+`;
 
 export const Name = styled.p`
   font-size: 20px;
@@ -20,6 +22,7 @@ export const Name = styled.p`
 export const Status = styled.span`
   width: 15px;
   height: 15px;
+  margin-right: 10px;
   border-radius: 100%;
-  background-color: rgb(34 161 161);
+  background-color: ${({ status }) => (status ? 'green' : 'red')};
 `;
